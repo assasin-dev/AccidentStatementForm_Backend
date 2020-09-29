b3e5015fa0b2fe61ff23c5058b672013d11b80af
assasin-dev <hhu5731@outlook.com>
2020-09-29 18:48:20 +0300

    added insert and select for driver B link

---
+++ env.php
@@ -0,0 +1,7 @@
+<?php
+// Define environment valiables
+$db_servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name = "accident_statement";
+?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require('env.php');
 
 function hashs(){
 	$priKeyId = openssl_get_privatekey(file_get_contents('privkey.pem'));
@@ -77,6 +78,45 @@ if(isset($_POST['ask_hash'])) {
 	//     echo $oEntry->OwnerName . "\n";
 	// }
 	 echo json_encode($oXML->GetVehicleMinimizedData);
-	// echo $oXML;
 }
+
+if(isset($_POST['a_info_request'])) {
+	// Create connection
+	$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+	// Check connection
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+	$photo_page = AddSlashes($_POST['photo_page']);
+	$scheme_page = AddSlashes($_POST['scheme_page']);
+	$review_page = AddSlashes($_POST['review_page']);
+	// $source_canvas = AddSlashes($_POST['source_canvas']);
+	$source_canvas = $_POST['source_canvas'];
+	$a_sign_canvas = $_POST['a_sign_canvas'];
+	$sql = "INSERT INTO information (a_reg_num, a_owner_phone, a_driver_name, a_driver_id_code, a_owner_name, a_driver_license, a_state_no, a_brand, a_model, a_owner_address, a_owner_email, event_type, a_kind, affected_vehicle, affected_assets, affected_person, b_license, b_phone, event_time, incident_place, event_lat, event_lng, event_description, registered_police, police_detail, a_was_driving, a_tpvca_insurer, a_want_kasko, a_kasko_insurer, photo_page, scheme_page, review_page, source_canvas, a_sign_canvas)
+	VALUES ('". $_POST['a_reg_num'] . "','" . $_POST['a_owner_phone'] . "','" . $_POST['a_driver_name'] . "','" . $_POST['a_driver_id_code'] . "','" . $_POST['a_owner_name'] . "','" . $_POST['a_driver_license'] . "','" . $_POST['a_state_no'] . "','" . $_POST['a_brand'] . "','" . $_POST['a_model'] . "','" . $_POST['a_owner_address'] . "','" . $_POST['a_owner_email'] . "','" . $_POST['event_type'] . "','" . $_POST['a_kind'] . "','" . $_POST['affected_vehicle'] . "','" . $_POST['affected_assets'] . "','" . $_POST['affected_person'] . "','" . $_POST['b_license'] . "','" . $_POST['b_phone'] . "','" . $_POST['event_time'] . "','" . $_POST['incident_place'] . "','" . $_POST['event_lat'] . "','" . $_POST['event_lng'] . "','" . $_POST['event_description'] . "','" . $_POST['registered_police'] . "','" . $_POST['police_detail'] . "','" . $_POST['a_was_driving'] . "','" . $_POST['a_tpvca_insurer'] . "','" . $_POST['a_want_kasko'] . "','" . $_POST['a_kasko_insurer'] . "','" . $photo_page . "','" . $scheme_page . "','" . $review_page . "','" . $source_canvas . "','" . $a_sign_canvas . "')";
+	$res = $conn->query($sql);
+	if ($res === TRUE) {
+	  echo "save_information_success";
+	} else {
+	  // echo "Error: " . $sql . "<br>" . $conn->error;
+		echo $_POST['photo_page'];
+		echo "save_information_failed";
+	}
+
+	$conn->close();
+}
+
+if(isset($_POST['b_request_information'])) {
+	$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+	// Check connection
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+	$sql = "SELECT * FROM information WHERE b_phone = '". $_POST['b_phone']."'";
+	$res = $conn->query($sql);
+	$result = $res->fetch_assoc();
+	echo json_encode($result);
+}
+
 ?>
