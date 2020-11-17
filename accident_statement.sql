76b864ef3ae223e86cd1dce5a81ecf75d0f78254
assasin-dev <hhu5731@outlook.com>
2020-11-17 17:53:23 +0800

    updated backend as additional issues 1-19

--- accident_statement.sql
+++ accident_statement.sql
@@ -1,63 +1,72 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : accident_statement
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 100138
+ Source Host           : localhost:3306
+ Source Schema         : accident_statement
 
-Target Server Type    : MYSQL
-Target Server Version : 50505
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 100138
+ File Encoding         : 65001
 
-Date: 2020-09-29 18:49:55
+ Date: 17/11/2020 17:52:50
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for information
 -- ----------------------------
 DROP TABLE IF EXISTS `information`;
-CREATE TABLE `information` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `a_reg_num` varchar(255) DEFAULT NULL,
-  `a_owner_phone` varchar(255) DEFAULT NULL,
-  `a_driver_name` varchar(255) DEFAULT NULL,
-  `a_driver_id_code` varchar(255) DEFAULT NULL,
-  `a_owner_name` varchar(255) DEFAULT NULL,
-  `a_driver_license` varchar(255) DEFAULT NULL,
-  `a_state_no` varchar(255) DEFAULT NULL,
-  `a_brand` varchar(255) DEFAULT NULL,
-  `a_model` varchar(255) DEFAULT NULL,
-  `a_owner_address` varchar(255) DEFAULT NULL,
-  `a_owner_email` varchar(255) DEFAULT NULL,
-  `event_type` varchar(255) DEFAULT NULL,
-  `a_kind` varchar(255) DEFAULT NULL,
-  `affected_vehicle` int(5) DEFAULT NULL,
-  `affected_assets` int(5) DEFAULT NULL,
-  `affected_person` int(5) DEFAULT NULL,
-  `b_license` varchar(255) DEFAULT NULL,
-  `b_phone` varchar(255) DEFAULT NULL,
-  `event_time` varchar(255) DEFAULT NULL,
-  `incident_place` varchar(255) DEFAULT NULL,
-  `event_lat` float DEFAULT NULL,
-  `event_lng` float DEFAULT NULL,
-  `event_description` longblob DEFAULT NULL,
-  `registered_police` varchar(255) DEFAULT NULL,
-  `police_detail` varchar(255) DEFAULT NULL,
-  `a_was_driving` varchar(255) DEFAULT NULL,
-  `a_tpvca_insurer` varchar(255) DEFAULT NULL,
-  `a_want_kasko` varchar(255) DEFAULT NULL,
-  `a_kasko_insurer` varchar(255) DEFAULT '',
-  `photo_page` longblob DEFAULT NULL,
-  `scheme_page` longblob DEFAULT NULL,
-  `review_page` longblob DEFAULT NULL,
-  `source_canvas` longblob DEFAULT NULL,
-  `a_sign_canvas` longblob DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `information`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `what_affected` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `injured_fullname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `injured_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `damaged_description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `asset_owner_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_reg_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_owner_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_driver_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_driver_id_code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_owner_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_driver_license` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_state_no` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_brand` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_owner_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_owner_email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `event_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_kind` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `affected_vehicle` int NULL DEFAULT NULL,
+  `affected_assets` int NULL DEFAULT NULL,
+  `affected_person` int NULL DEFAULT NULL,
+  `b_license` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `b_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `event_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `incident_place` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `event_lat` float NULL DEFAULT NULL,
+  `event_lng` float NULL DEFAULT NULL,
+  `event_description` longblob NULL,
+  `registered_police` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `police_detail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_was_driving` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_tpvca_insurer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_want_kasko` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `a_kasko_insurer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '',
+  `photo_page` longblob NULL,
+  `scheme_page` longblob NULL,
+  `review_page` longblob NULL,
+  `source_canvas` longblob NULL,
+  `a_sign_canvas` longblob NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of information
 -- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
--- index.php
+++ index.php
@@ -25,59 +25,63 @@ function hashs(){
 $hash = hashs();
 // var_dump($_REQUEST);
 // echo $_POST['ask_hash'];
-if(isset($_POST['ask_hash'])) {
-// 	echo "start";
-	$reg_num = $_POST['reg_num'];
-	$owner_code = $_POST['owner_code'];
+if(isset($_POST['b_affected'])) {
+	if($_POST['b_affected'] == 'injured' || $_POST['b_affected'] == 'asset') {
+		echo $_POST['b_affected'];
+	} else {
+	// 	echo "start";
+		$reg_num = $_POST['reg_num'];
+		$owner_code = $_POST['owner_code'];
 
-	//The XML string that you want to send.
-	$xml = "<Request><Login>
-	        <LoginName>eaccident@edrauda.lt</LoginName>
-	        <Password>UbcHjH8HxNekaEys</Password>
-	        <Hash>" . $hash . "</Hash></Login>
-	        <GetVehicleMinimizedData>
-	        <Source>any</Source>
-	        <AutoNumber>" . $reg_num . "</AutoNumber>
-	        <OwnerCode>" . $owner_code . "</OwnerCode>
-	        </GetVehicleMinimizedData></Request>";
-	 
-	 
-	//The URL that you want to send your XML to.
-	$url = 'https://www.drauskaita.lt/services/getVehicleMinimizedData';
-	 
-	//Initiate cURL
-	$curl = curl_init($url);
-	 
-	//Set the Content-Type to text/xml.
-	curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
-	 
-	//Set CURLOPT_POST to true to send a POST request.
-	curl_setopt($curl, CURLOPT_POST, true);
-	 
-	//Attach the XML string to the body of our request.
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $xml);
-	 
-	//Tell cURL that we want the response to be returned as
-	//a string instead of being dumped to the output.
-	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	 
-	//Execute the POST request and send our XML.
-	$result = curl_exec($curl);
-	 
-	//Do some basic error checking.
-	if(curl_errno($curl)){
-	    throw new Exception(curl_error($curl));
+		//The XML string that you want to send.
+		$xml = "<Request><Login>
+		        <LoginName>eaccident@edrauda.lt</LoginName>
+		        <Password>UbcHjH8HxNekaEys</Password>
+		        <Hash>" . $hash . "</Hash></Login>
+		        <GetVehicleMinimizedData>
+		        <Source>any</Source>
+		        <AutoNumber>" . $reg_num . "</AutoNumber>
+		        <OwnerCode>" . $owner_code . "</OwnerCode>
+		        </GetVehicleMinimizedData></Request>";
+		 
+		 
+		//The URL that you want to send your XML to.
+		$url = 'https://www.drauskaita.lt/services/getVehicleMinimizedData';
+		 
+		//Initiate cURL
+		$curl = curl_init($url);
+		 
+		//Set the Content-Type to text/xml.
+		curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
+		 
+		//Set CURLOPT_POST to true to send a POST request.
+		curl_setopt($curl, CURLOPT_POST, true);
+		 
+		//Attach the XML string to the body of our request.
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $xml);
+		 
+		//Tell cURL that we want the response to be returned as
+		//a string instead of being dumped to the output.
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		 
+		//Execute the POST request and send our XML.
+		$result = curl_exec($curl);
+		 
+		//Do some basic error checking.
+		if(curl_errno($curl)){
+		    throw new Exception(curl_error($curl));
+		}
+		 
+		//Close the cURL handle.
+		curl_close($curl);
+		 $oXML = new SimpleXMLElement($result);
+		//Print out the response output.
+		// var_dump($oXML);
+		// foreach($oXML->GetVehicleMinimizedData as $oEntry){
+		//     echo $oEntry->OwnerName . "\n";
+		// }
+		 echo json_encode($oXML->GetVehicleMinimizedData);
 	}
-	 
-	//Close the cURL handle.
-	curl_close($curl);
-	 $oXML = new SimpleXMLElement($result);
-	//Print out the response output.
-	// var_dump($oXML);
-	// foreach($oXML->GetVehicleMinimizedData as $oEntry){
-	//     echo $oEntry->OwnerName . "\n";
-	// }
-	 echo json_encode($oXML->GetVehicleMinimizedData);
 }
 
 if(isset($_POST['a_info_request'])) {
@@ -93,14 +97,14 @@ if(isset($_POST['a_info_request'])) {
 	// $source_canvas = AddSlashes($_POST['source_canvas']);
 	$source_canvas = $_POST['source_canvas'];
 	$a_sign_canvas = $_POST['a_sign_canvas'];
-	$sql = "INSERT INTO information (a_reg_num, a_owner_phone, a_driver_name, a_driver_id_code, a_owner_name, a_driver_license, a_state_no, a_brand, a_model, a_owner_address, a_owner_email, event_type, a_kind, affected_vehicle, affected_assets, affected_person, b_license, b_phone, event_time, incident_place, event_lat, event_lng, event_description, registered_police, police_detail, a_was_driving, a_tpvca_insurer, a_want_kasko, a_kasko_insurer, photo_page, scheme_page, review_page, source_canvas, a_sign_canvas)
-	VALUES ('". $_POST['a_reg_num'] . "','" . $_POST['a_owner_phone'] . "','" . $_POST['a_driver_name'] . "','" . $_POST['a_driver_id_code'] . "','" . $_POST['a_owner_name'] . "','" . $_POST['a_driver_license'] . "','" . $_POST['a_state_no'] . "','" . $_POST['a_brand'] . "','" . $_POST['a_model'] . "','" . $_POST['a_owner_address'] . "','" . $_POST['a_owner_email'] . "','" . $_POST['event_type'] . "','" . $_POST['a_kind'] . "','" . $_POST['affected_vehicle'] . "','" . $_POST['affected_assets'] . "','" . $_POST['affected_person'] . "','" . $_POST['b_license'] . "','" . $_POST['b_phone'] . "','" . $_POST['event_time'] . "','" . $_POST['incident_place'] . "','" . $_POST['event_lat'] . "','" . $_POST['event_lng'] . "','" . $_POST['event_description'] . "','" . $_POST['registered_police'] . "','" . $_POST['police_detail'] . "','" . $_POST['a_was_driving'] . "','" . $_POST['a_tpvca_insurer'] . "','" . $_POST['a_want_kasko'] . "','" . $_POST['a_kasko_insurer'] . "','" . $photo_page . "','" . $scheme_page . "','" . $review_page . "','" . $source_canvas . "','" . $a_sign_canvas . "')";
+	$sql = "INSERT INTO information (what_affected, injured_fullname, injured_phone, damaged_description, asset_owner_phone, a_reg_num, a_owner_phone, a_driver_name, a_driver_id_code, a_owner_name, a_driver_license, a_state_no, a_brand, a_model, a_owner_address, a_owner_email, event_type, a_kind, affected_vehicle, affected_assets, affected_person, b_license, b_phone, event_time, incident_place, event_lat, event_lng, event_description, registered_police, police_detail, a_was_driving, a_tpvca_insurer, a_want_kasko, a_kasko_insurer, photo_page, scheme_page, review_page, source_canvas, a_sign_canvas)
+	VALUES ('". $_POST['what_affected'] . "','". $_POST['injured_fullname'] . "','". $_POST['injured_phone'] . "','". $_POST['damaged_description'] . "','". $_POST['asset_owner_phone'] . "','". $_POST['a_reg_num'] . "','" . $_POST['a_owner_phone'] . "','" . $_POST['a_driver_name'] . "','" . $_POST['a_driver_id_code'] . "','" . $_POST['a_owner_name'] . "','" . $_POST['a_driver_license'] . "','" . $_POST['a_state_no'] . "','" . $_POST['a_brand'] . "','" . $_POST['a_model'] . "','" . $_POST['a_owner_address'] . "','" . $_POST['a_owner_email'] . "','" . $_POST['event_type'] . "','" . $_POST['a_kind'] . "','" . $_POST['affected_vehicle'] . "','" . $_POST['affected_assets'] . "','" . $_POST['affected_person'] . "','" . $_POST['b_license'] . "','" . $_POST['b_phone'] . "','" . $_POST['event_time'] . "','" . $_POST['incident_place'] . "','" . $_POST['event_lat'] . "','" . $_POST['event_lng'] . "','" . $_POST['event_description'] . "','" . $_POST['registered_police'] . "','" . $_POST['police_detail'] . "','" . $_POST['a_was_driving'] . "','" . $_POST['a_tpvca_insurer'] . "','" . $_POST['a_want_kasko'] . "','" . $_POST['a_kasko_insurer'] . "','" . $photo_page . "','" . $scheme_page . "','" . $review_page . "','" . $source_canvas . "','" . $a_sign_canvas . "')";
 	$res = $conn->query($sql);
 	if ($res === TRUE) {
 	  echo "save_information_success";
 	} else {
-	  // echo "Error: " . $sql . "<br>" . $conn->error;
-		echo $_POST['photo_page'];
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+		// echo $_POST['photo_page'];
 		echo "save_information_failed";
 	}
 
